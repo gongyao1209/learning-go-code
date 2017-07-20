68d623a388e58dd20b07ab518245f3606b7de4b0
gongyao <gongyao@example.com>
2017-07-20 21:44:20 +0800

    [提交代码]

--- chapter1/channels/hellochannels.go
+++ chapter1/channels/hellochannels.go
@@ -9,12 +9,22 @@ var wg sync.WaitGroup
 
 func printer(ch chan int) {
 	for i := range ch {
-		fmt.Printf("Received %d ", i)
+		fmt.Printf("Received %d \n", i)
+	}
+	wg.Done()
+}
+
+func printer1(ch chan int) {
+	for i := range ch{
+		fmt.Printf("gongyao %d\n", i)
 	}
 	wg.Done()
 }
 
 // main is the entry point for the program.
+/*
+	Go和C很一样，你只用你的东西，它的东西你都不要动，也不要想看看其中有什么。会发生未知的错误
+ */
 func main() {
 	c := make(chan int)
 	go printer(c)
