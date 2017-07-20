989434f111a01702ac3abb16042253aa321a291c
gongyao <gongyao@example.com>
2017-07-20 17:49:43 +0800

    [提交代码]

--- chapter2/sample/main.go
+++ chapter2/sample/main.go
@@ -19,3 +19,11 @@ func main() {
 	// Perform the search for the specified term.
 	search.Run("president")
 }
+
+/**
+	1、每个代码文件都属于一个包，包名应该与代码文件所在的文件夹同名；
+	2、Go可以先声明再初始化，也可以初始化的时候声明
+	3、使用指针可以在函数件或者 goroutine 间共享数据
+	4、goroutine 来并发和同步
+	5、Go是面向方法的，和C一样可以建立自己的struct
+ */
