0ba9b5ef190301b0e8fd7c881f5031f7ca44efeb
gongyao <gongyao@example.com>
2017-08-08 17:45:13 +0800

    [提交代码]

--- chapter5/listing11/listing11.go
+++ chapter5/listing11/listing11.go
@@ -24,6 +24,10 @@ func (u user) notify() {
 		u.email)
 }
 
+func (u user) change() {
+	u.name = "gongyao"
+}
+
 // changeEmail implements a method with a pointer receiver.
 func (u *user) changeEmail(email string) {
 	u.email = email
@@ -43,43 +47,13 @@ func main() {
 
 	// Values of type user can be used to call methods
 	// declared with a pointer receiver.
-	bill.changeEmail("bill@newdomain.com")
-	bill.notify()
+	//bill.changeEmail("bill@newdomain.com")
+	//bill.notify()
 
 	// Pointers of type user can be used to call methods
 	// declared with a pointer receiver.
-	lisa.changeEmail("lisa@newdomain.com")
-	lisa.notify()
-
-	//已经初始化了
-	gyao := user{
-		email:"gongyao1992@qq.com",
-		name:"gongyao",
-	}
-	//gyao1 := user{"gongyao1", "gongyao1992@qq.com"}
-
-	gyao.notify()
-	gyao.changeName("wanghui")
-	gyao.notify()
-	//gyao1.notify()
-
-	gyao2 := admin{
-		people:gyao,
-		age:25,
-	}
-
-	gyao2.people.notify()
-	gyao2.people.changeName("yaoke")
-	gyao2.people.notify()
-
-	gyao3 := admin{
-		people:user{
-			name:"haonan",
-			email:"heshun@qq.com",
-		},
-		age:21,
-	}
-	gyao3.notify2()
+	//lisa.changeEmail("lisa@newdomain.com")
+	//lisa.notify()
 }
 
 func (u *user)changeName(new_name string) {
