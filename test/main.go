00baad3b8637cf50041c06dcb5e5d142701fda3f
gongyao <gongyao@example.com>
2017-08-22 18:27:07 +0800

    [提交代码]

--- test/main.go
+++ test/main.go
@@ -1,24 +1,51 @@
 package main
 
 import (
-	chap5 "../test/chapter5"
+	//chap5 "../test/chapter5"
 	"fmt"
-	"runtime"
+	//"runtime"
+	"os"
+	"log"
 )
 
 func main()  {
-	name2 := string("gongyao")
-	fmt.Printf("%s", name2)
-	chap5.Test(name2)
+	//name2 := string("gongyao")
+	//fmt.Printf("%s", name2)
+	//chap5.Test(name2)
+	//
+	//var yaoke chap5.User
+	//yaoke.SetName("yaoke")
+	//yaoke.SetMobile("186")
+	//yaoke.SetMobile1("176")
+	//yaoke.Show()
+	//
+	//fmt.Printf("Cpu number is :%d\n", runtime.NumCPU())
+	//
+	//fmt.Print(chap5.New(12))
 
-	var yaoke chap5.User
-	yaoke.SetName("yaoke")
-	yaoke.SetMobile("186")
-	yaoke.SetMobile1("176")
-	yaoke.Show()
+	file_name := "/Users/gongyao/workspace/go_code1/code/chapter3/wordcount/gowords.txt"
+	file, err := os.Open(file_name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Printf("Cpu number is :%d\n", runtime.NumCPU())
+	fmt.Printf("Read %d bytes: %q\n", count, data[:count])
 
-	fmt.Print(chap5.New(12))
+	os.Getpagesize()
+
+	fmt.Printf("%d", os.Getpagesize())
+
+	//src := io.Reader("aaa")
+	//var s scanner.Scanner
+	//
+	//s.Init(src)
+	//tok := s.Scan()
+	//for tok != scanner.EOF {
+	//	tok = s.Scan()
+	//}
 }
-
